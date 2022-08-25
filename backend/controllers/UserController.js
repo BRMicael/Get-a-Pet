73b5c0f391bc8165d1348308b70171479c24e462
Micael Batista <micaelbatista0108@hotmail.com>
2022-08-25 11:57:49 -0300

    token verfication process created

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -110,5 +110,41 @@ module.exports = class UserController {
         res.status(200).send(currentUser)
     }
 
+    static async getUserById(req, res) {
+        const id = req.params.id
 
+        const user = await user.findById(id)
+
+        if(!user) {
+            res.status(422).json({
+                message: 'Usuário não encontrado'
+            })
+            return
+        }
+
+        res.status(200).json({user})
+    }
+
+    static async getUserById(req, res){
+
+        const id = req.params.id
+
+        const user = await User.findById(id).select('-password')
+
+        if(!user){
+            res.status(422).json({
+                message: 'Usuário não encontrado'
+            })
+            return
+        }
+
+        res.status(200).json({user})
+    }
+
+    static async editUser(req, res) {
+        res.status(200).json({
+            message: 'Deu certo update!'
+        })
+        return
+    }
 }
