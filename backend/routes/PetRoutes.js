0fde9cc4ae9fcd6642268ab90e9cd59d2e5e0280
Micael Batista <micaelbatista0108@hotmail.com>
2022-08-26 20:25:05 -0300

    funcionalidades de resgate de pets feita

--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -4,6 +4,10 @@ const PetController = require('../controllers/PetController');
 const verifyToken = require('../helpers/verify-token');
 const {imageUpload} = require('../helpers/image-upload')
 
+router.get('/', PetController.getAll);
 router.post('/create', verifyToken, imageUpload.array('images'), PetController.create);
+router.get('/mypets', verifyToken, PetController.getAllUserPets);
+router.get('/myadoptions', verifyToken, PetController.getAllUserAdoptions);
+router.get('/:id', PetController.getPetById)
 
 module.exports = router;
