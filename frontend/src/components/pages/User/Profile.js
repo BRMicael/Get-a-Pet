854f4d95d5a6e08a7b29920abcb7274b1f21f332
Micael Batista <micaelbatista0108@hotmail.com>
2022-09-02 23:18:06 -0300

    resgantando dados no form de editar

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import style from "./Profile.module.css";
+import formStyles from "../../form/Form.module.css";
+import Input from "../../form/Input";
+import api from "../../../utils/api";
 
 export default function Profile() {
+    const [user, setUser] = useState({});
+    const [token] = useState(localStorage.getItem('token') || '');
+
+  useEffect(() => {
+    api.get('/users/checkuser' ,{
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`
+        }
+    }).then((response) => {
+      setUser(response.data);
+    })
+  }, [token])
+
+    function handleChange(e){}
+    function onFileChange(e){}
+
+
   return (
-    <div>Profile</div>
-  )
+    <div>
+      <div className={style.profile_header}>
+        <h1>Perfil</h1>
+        <p>Preview</p>
+      </div>
+      <form className={formStyles.form_container}>
+        <Input
+          text="Imagem"
+          type="file"
+          name="image"
+          handleOnChange={onFileChange}
+        />
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite seu nome"
+          handleOnChange={handleChange}
+          value={user.name || ''}
+        />
+        <Input
+          text="Email"
+          type="email"
+          name="email"
+          placeholder="Digite seu email"
+          handleOnChange={handleChange}
+          value={user.email || ''}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite seu nome telefone"
+          handleOnChange={handleChange}
+          value={user.phone || ''}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirmação de senha"
+          type="password"
+          name="confirmpassword"
+          placeholder="Digite sua senha novamente"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Editar" />
+      </form>
+    </div>
+  );
 }
