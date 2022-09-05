45c033d8bf61784c7c187a9e829872bbc2fe4432
Micael Batista <micaelbatista0108@hotmail.com>
2022-09-05 00:30:23 -0300

    update user interface and function completed

--- frontend/src/components/pages/User/Profile.js
+++ frontend/src/components/pages/User/Profile.js
@@ -3,10 +3,13 @@ import style from "./Profile.module.css";
 import formStyles from "../../form/Form.module.css";
 import Input from "../../form/Input";
 import api from "../../../utils/api";
+import useFlashMessage from '../../../hooks/useFlashMessage'
 
 export default function Profile() {
     const [user, setUser] = useState({});
+    const [preview, setPreview] = useState();
     const [token] = useState(localStorage.getItem('token') || '');
+    const {setFlashMessage} = useFlashMessage();
 
   useEffect(() => {
     api.get('/users/checkuser' ,{
@@ -18,17 +21,54 @@ export default function Profile() {
     })
   }, [token])
 
-    function handleChange(e){}
-    function onFileChange(e){}
+    function handleChange(e){
+      setUser({...user, [e.target.name]: e.target.value})
+    }
+    function onFileChange(e){
+      setPreview(e.target.files[0])
+      setUser({...user, [e.target.name]: e.target.files[0]})
+    }
 
+    async function handleSubmit(e){
+      e.preventDefault();
+
+      let msgType = '';
+
+      const formData = new FormData();
+      await Object.keys(user).forEach((key) => formData.append(key, user[key]))
+
+
+
+      const data = await api.patch(`/users/edit/${user._id}`, formData, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+          'Content-Type': 'multipart/form-data'
+        }
+      }).then((response) => {
+        msgType = 'success';
+        return response.data
+      }).catch((err) => {
+        msgType = 'error';
+        return err.response.data;
+      })
+
+      setFlashMessage(data.message, msgType);
+    }
 
   return (
     <div>
       <div className={style.profile_header}>
-        <h1>Perfil</h1>
-        <p>Preview</p>
+      <h1>Perfil</h1>
+        {(user.image || preview) && (
+          <img src={
+            preview ? URL.createObjectURL(preview)
+            : `${process.env.REACT_APP_API}/images/users/${user.image}`
+          } 
+          alt={user.name}
+          />
+        )}
       </div>
-      <form className={formStyles.form_container}>
+      <form onSubmit={handleSubmit} className={formStyles.form_container}>
         <Input
           text="Imagem"
           type="file"
