8373ddffd4de5297de9a37cf98bcadae9ce08f2a
Micael Batista <micaelbatista0108@hotmail.com>
2022-09-06 23:27:22 -0300

    mypets page interface completed

--- frontend/src/components/pages/Pets/MyPets.js
+++ frontend/src/components/pages/Pets/MyPets.js
@@ -1,17 +1,55 @@
 import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import RoundedImage from '../../layout/RoundedImage';
+import api from '../../../utils/api';
+import styles from './Dashboard.module.css'
+
+import useFlashMessage from '../../../hooks/useFlashMessage';
 
 export default function MyPets() {
   const [pets, setPets] = useState([])
-  
+  const [token] = useState(localStorage.getItem('token') || '')
+  const {setFlashMessage} = useFlashMessage()
+
+  useEffect(() => {
+    api.get('/pets/mypets', {
+      headers: {
+        Authorization: `Bearer ${JSON.parse(token)}`
+      }
+    })
+    .then((response) => {
+      setPets(response.data.pets)
+    })
+  }, [token])
+
     return (
-    <div>
-        <div>
+    <div >
+        <div className={styles.petlist_header}>
             <h1>MyPets</h1>
             <Link to="/pet/add">Cadastrar Pet</Link>
         </div>
-        <div>
-            {pets.length > 0 && <p>Meus pets cadastrados</p>}
+        <div className={styles.petlist_container}>
+            {pets.length > 0 && 
+              pets.map((pet) => (
+              <div key={pet._id} className={styles.petlist_row}>
+                <RoundedImage
+                src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
+                alt={pet.name}
+                width="px75"
+                />
+                <span className="bold">{pet.name}</span>
+                <div className={styles.actions}>
+                  {pet.available ? 
+                  (<>
+                    {pet.adopter && <button className={styles.conclude_btn}>Concluir adoção</button>}
+                    <Link to={`/pet/edit/${pet._id}`}>Editar</Link>
+                    <button>Excluir</button>
+                  </>) : 
+                  (<p>Já foi adotado</p>) }
+                </div>
+              </div>
+              ))
+            }
             {pets.length === 0 && <p>Não há pets cadastrados</p>}
         </div>
     </div>
