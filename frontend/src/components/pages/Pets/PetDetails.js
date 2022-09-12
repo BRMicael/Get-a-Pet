f5bdc36fd5ffa6c95b841befaef7319e4581712f
Micael Batista <micaelbatista0108@hotmail.com>
2022-09-11 22:29:25 -0300

    page that show all pets registered created

---
+++ frontend/src/components/pages/Pets/PetDetails.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import styles from './PetDetails.module.css';
+import api from '../../../utils/api';
+import {useParams, Link} from 'react-router-dom';
+
+import useFlashMessage from '../../../hooks/useFlashMessage';
+
+export default function PetDetails() {
+  return (
+    <div>PetDetails</div>
+  )
+}
